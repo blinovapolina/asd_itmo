396adc9a93248426de5a422d9bbaea5fb992d8c9
blinovapolina <blinovapolina06@yandex.ru>
2024-12-07 06:25:35 +0300

    made run_all_test_lab.py + remade run_all_lab.py + rename run_tests.py

---
+++ run_all_test_lab.py
@@ -0,0 +1,19 @@
+from lab2.run_tests import run_all_tests as lab2
+from lab3.run_tests import run_all_tests as lab3
+
+
+def main():
+    labs = {
+        "Лабораторная №2": (lab2, 1),
+        "Лабораторная №3": (lab3,1),
+    }
+
+    for lab_name, (lab_func, path) in labs.items():
+        print('====================')
+        print(lab_name)
+        print('====================')
+        lab_func()
+        print()
+
+if __name__ == "__main__":
+    main()
